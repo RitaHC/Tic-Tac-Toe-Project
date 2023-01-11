ff568eaf3b10556afc132c1e615d61400f38ecbb
Rita HC <ritahameerchauhan@gmail.com>
2023-01-10 21:58:33 -0500

    Comments added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 console.log('Hello Hameer')
 
+/////////////// Helper variebles /////////////////
 let turn = 'X'
 let gameover = false
 const winningImage = document.querySelector('img')
@@ -7,16 +8,17 @@ let boxes = document.querySelectorAll('.box')
 const container = document.querySelector('#container')
 let turnCount = 0
 
-//Change Turn 
+/////// Change Turn Function ///////////////////// 
 const changeTurn = () => {
     turnCount ++
     return turn==='X'? '0': 'X'
 } 
 
 
-//Function to checkwin
+/////// Function to checkwin //////////////////////
 const checkWin = () => {
     let boxtext = document.getElementsByClassName('boxtext')
+    // All win conditions have been made into an array 
     let wins = [
         [0, 1, 2], 
         [3, 4, 5], 
@@ -27,15 +29,9 @@ const checkWin = () => {
         [0, 4, 8], 
         [2, 4, 6]
     ]
-
-    // if (boxtext){
-    //     console.log(`It's a Tie`)
-    // }
     
-
+    //Checking if it's a win ----- Checking if all elements of an array are the same
     wins.forEach (e=>{
-
-        
         if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !=='')){
             document.querySelector('.info').innerText = boxtext[e[0]].innerText + ' Won'
             gameover = true
@@ -45,7 +41,7 @@ const checkWin = () => {
                 console.log('disabling clicks')
         } 
         )
-        console.log('removing clicks')
+        // Draw Condition
          } 
          if (turnCount>=9){
             console.log(`It's a Draw`)
@@ -54,19 +50,13 @@ const checkWin = () => {
             turnCount = 0
 
          }
-
-        //  else if ((boxtext[e[0]].innerText !=='') && (boxtext[e[1]].innerText !=='') && (boxtext[e[2]].innerText !=='')) {
-        //      
-        //  }
-
-
 })
 
 }
 
-// Game Logic
+///////////////////////// Game Logic ///////////////////////////
 
-// Click Listener
+// Making boxes show 'X' or 'O' upon clicking
 Array.from(boxes).forEach (element => {
     let boxtext = element.querySelector('.boxtext')
 
@@ -76,10 +66,8 @@ Array.from(boxes).forEach (element => {
             boxtext.innerText = turn
             turn = changeTurn()
             console.log(turnCount)
-
-            
-            
             checkWin()
+            //Checking if the game is over
             if (!gameover){
                 document.getElementsByClassName('info')[0].innerText = `Turn for ${turn}`
             }
@@ -89,16 +77,7 @@ Array.from(boxes).forEach (element => {
 
 })
 
-// onclick listener to reset button
-
+////////// Onclick listener to reset button ////////////////////
 reset.addEventListener('click', () => {
     location.reload()
 } )
-
-const rajat = () => {
-    const print = document.createElement('h1')
-            print.innerText = 'Print Line'
-            const reset = document.getElementsByClassName('info')
-            info.appendChild(print)
-            console.log(print)
-}
